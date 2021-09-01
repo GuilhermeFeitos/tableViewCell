edd087b0878d10e400861cfa093212c5b3da4e7b
Gui Feitosa <guifeitos@gmail.com>
2021-09-01 18:46:51 -0300

    Criação da função abaixo para poder deletar uma celula durante a execução do programa em tela:
    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)

--- tableViewCell/tableViewCell/ViewController.swift
+++ tableViewCell/tableViewCell/ViewController.swift
@@ -56,8 +56,17 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        return true
+        
+    }
     
-    
-    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        programadores.remove(at: indexPath.row)
+        tableView.reloadData()
+        
+    }
     
 }
